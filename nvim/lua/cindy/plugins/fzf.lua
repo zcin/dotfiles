d79c522f19bdd04482039fcc8a36e6a59dd1f577
Cindy Zhang <cindyzyx9@gmail.com>
2024-08-03 21:50:52 -0700

    fuzzy finder keymaps
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- nvim/lua/cindy/plugins/fzf.lua
+++ nvim/lua/cindy/plugins/fzf.lua
@@ -7,7 +7,8 @@ return {
         require("fzf-lua").setup({})
 
         vim.keymap.set("n", "<C-p>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
-        vim.keymap.set("n", "<leader>ps", "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
-        vim.keymap.set("n", "<leader>pc", "<cmd>lua require('fzf-lua').grep_cword()<CR>", { silent = true })
+        vim.keymap.set("n", "<leader>fs", "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
+        vim.keymap.set("n", "<leader>fc", "<cmd>lua require('fzf-lua').grep_cword()<CR>", { silent = true })
+        vim.keymap.set("n", "<leader>fr", "<cmd>lua require('fzf-lua').lsp_references()<CR>", { silent = true })
     end
 }
